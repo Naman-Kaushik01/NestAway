eaba749778b4bf1665d4424600490fdc9db71ef1
Naman Kaushik <kaushiknaman68@gmail.com>
2025-10-07 01:18:17 +0530

    initalized data

--- models/listing.js
+++ models/listing.js
@@ -1,31 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const listingSchema = new Schema({
-    title:{
-        type : String,
-        required : true,
-    },
-    description : {
-        type : String
-    },
-    image : {
-        type : String,
-        default : "https://www.vecteezy.com/photo/12168187-beautiful-sunset-on-the-beach-with-palm-tree-for-travel-and-vacation",
-        set : (v) => v === ""
-        ? "https://www.vecteezy.com/photo/12168187-beautiful-sunset-on-the-beach-with-palm-tree-for-travel-and-vacation" : v,
-    },
-    price : {
-        type : Number,
-    },
-    loction : {
-        type : String,
-    },
-    country : {
-        type : String,
-    }
 
-})
+const listingSchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  image: {
+    filename: String,
+    url: {
+      type: String,
+      default: "https://images.unsplash.com/photo-1625505826533-...",
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
 
 
 //Creating collection in db.
